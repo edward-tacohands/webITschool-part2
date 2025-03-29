fff67b33888a65f3d85579e05fc9168625aa17c9
edward-tacohands <eddierecochiropractic@gmail.com>
2025-03-29 10:00:06 +0100

    first commit

---
+++ data/courses.js
@@ -0,0 +1,62 @@
+export const courses = [
+    {
+        id: 1,
+        title: 'Systemutveckling inriktning A-kassa' ,
+        duration: 20,
+        adress: 'Malmö',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image1.jpg',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, culpa sunt in eveniet aliquid perferendis veniam hic assumenda fuga minima.'
+    },
+    {
+        id: 2,
+        title: 'Webbutveckling JavaScript' ,
+        duration: 20,
+        adress: 'Luleå',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image2.jpg',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, culpa sunt in eveniet aliquid perferendis veniam hic assumenda fuga minima.'
+    },
+    {
+        id: 3,
+        title: 'DevWhoops :P' ,
+        duration: 20,
+        adress: 'Distans',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image3.jpg',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, culpa sunt in eveniet aliquid perferendis veniam hic assumenda fuga minima.'
+    },
+    {
+        id: 4,
+        title: 'Cloud Developer' ,
+        duration: 20,
+        adress: 'Distans',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image4.jpg',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, culpa sunt in eveniet aliquid perferendis veniam hic assumenda fuga minima.'
+    },
+    {
+        id: 5,
+        title: 'Hovslagare' ,
+        duration: 20,
+        adress: 'Malmö',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image5.jpg',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, culpa sunt in eveniet aliquid perferendis veniam hic assumenda fuga minima.'
+    },
+    {
+        id: 6,
+        title: 'Content Creator' ,
+        duration: 20,
+        adress: 'Stockholm',
+        extent: '100%',
+        applyBefore: '31 mars 2026',
+        imageUrl: 'image6.jpg',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, culpa sunt in eveniet aliquid perferendis veniam hic assumenda fuga minima.'
+    }
+]
